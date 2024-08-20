b7be31b23d702c27cabdd00b7ce5313a46056804
Princess Elissa <tsekouras.elissa@gmail.com>
2024-08-20 17:35:58 +0200

    Move isStarred and isOnWatchList logic up to parent component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,10 +4,8 @@ import watchLaterSlice from '../data/watchLaterSlice'
 import placeholder from '../assets/not-found-500X750.jpeg'
 import { useCallback, useState } from 'react'
 
-const Movie = ({ movie, viewTrailer }) => {
+const Movie = ({ movie, viewTrailer, isStarred, isOnWatchList }) => {
 
-    const starredList = useSelector((state) => state.starred.starredMovies)
-    const watchLaterList = useSelector((state) => state.watchLater.watchLaterMovies)
     const { starMovie, unstarMovie } = starredSlice.actions
     const { addToWatchLater, removeFromWatchLater } = watchLaterSlice.actions
     const [isOpen, setOpen] = useState(false);
@@ -35,33 +33,19 @@ const Movie = ({ movie, viewTrailer }) => {
                 <div className="info_panel">
                     <div className="overview">{movie.overview}</div>
                     <div className="year">{movie.release_date?.substring(0, 4)}</div>
-                    {!starredList.map(movie => movie.id).includes(movie.id) ? (
-                        <span className="btn-star" data-testid="starred-link" onClick={() => 
-                            dispatch(starMovie({
-                                id: movie.id, 
-                                overview: movie.overview, 
-                                release_date: movie.release_date?.substring(0, 4),
-                                poster_path: movie.poster_path,
-                                title: movie.title
-                            })
-                        )}>
-                            <i className="bi bi-star" />
-                        </span>
-                    ) : (
+                    {isStarred ? (
                         <span className="btn-star" data-testid="unstar-link" onClick={() => dispatch(unstarMovie(movie))}>
                             <i className="bi bi-star-fill" data-testid="star-fill" />
                         </span>
-                    )}
-                    {!watchLaterList.map(movie => movie.id).includes(movie.id) ? (
-                        <button type="button" data-testid="watch-later" className="btn btn-light btn-watch-later" onClick={() => dispatch(addToWatchLater({
-                                id: movie.id, 
-                                overview: movie.overview, 
-                                release_date: movie.release_date?.substring(0, 4),
-                                poster_path: movie.poster_path,
-                                title: movie.title
-                        }))}>Watch Later</button>
                     ) : (
+                        <span className="btn-star" data-testid="starred-link" onClick={() => dispatch(starMovie(movie))}>
+                            <i className="bi bi-star" />
+                        </span>
+                    )}
+                    {isOnWatchList ? (
                         <button type="button" data-testid="remove-watch-later" className="btn btn-light btn-watch-later blue" onClick={() => dispatch(removeFromWatchLater(movie))}><i className="bi bi-check"></i></button>
+                    ) : (
+                        <button type="button" data-testid="watch-later" className="btn btn-light btn-watch-later" onClick={() => dispatch(addToWatchLater(movie))}>Watch Later</button>
                     )}
                     <button type="button" className="btn btn-dark" onClick={() => viewTrailer(movie)}>View Trailer</button>                                                
                 </div>
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,20 @@
 import Movie from './Movie'
 import { useSelector } from 'react-redux'
 import '../styles/movies.scss'
+import { useMemo } from 'react';
 
 const Movies = ({ viewTrailer }) => {
-    const movies = useSelector((state) => state.movies)
+    const movies = useSelector((state) => state.movies);
+    const starredList = useSelector((state) => state.starred.starredMovies);
+    const watchLaterList = useSelector((state) => state.watchLater.watchLaterMovies);
+
+    const starredIds = useMemo(() => {
+        return starredList.map(movie => movie.id)
+    }, [starredList]);
+
+    const watchLaterIds = useMemo(() => {
+        return watchLaterList.map(movie => movie.id)
+    }, [watchLaterList])
 
     return (
         <div data-testid="movies">
@@ -13,6 +24,8 @@ const Movies = ({ viewTrailer }) => {
                         movie={movie} 
                         key={movie.id}
                         viewTrailer={viewTrailer}
+                        isStarred={starredIds.includes(movie.id)}
+                        isOnWatchList={watchLaterIds.includes(movie.id)}
                     />
                 )
             })}
